7425b956b1878482ddd5d9bbde6c3283516342d6
Glitch (hello-express) <none>
2020-09-04 08:18:38 +0000

    👢🏅 Checkpoint ./server.js:9870244/1423

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const client = new discord.Client();
 client.login(process.env.TOKEN)
 //__________Bot login__________________\\
 client.on('ready', () => {
-    client.user.setActivity(`I'm Tom Bombadil`, { type: 'PLAYING' });
-    console.log('The Bot Is Online Im In Minecraft-Israel')
+    client.user.setActivity(`𝕀'𝕞 𝕋𝕠𝕞 𝔹𝕠𝕞𝕓𝕒𝕕𝕚𝕝 𝔸𝕟𝕕 𝕀'𝕞 𝕎𝕒𝕥𝕔𝕙𝕚𝕟𝕘 ${client.users.size} 𝕌𝕤𝕖𝕣𝕤`, { type: 'PLAYING' });
+    console.log('Tom Bombadil Is Up!')
 });
 //____________________________________________________Bot Playing___________________________________________________________\\
+client.on('message', message => {
+  if (message.content === '!invite') {
+    message.channel.send(`My Invite Link Is: `).catch();
+  }
+})
+//_____________________________commands________________________________\\
